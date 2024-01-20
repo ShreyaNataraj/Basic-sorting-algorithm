e85cfb024ec7030abae94858c1cc3551b9be16e5
Shreya N <nshreya094@gmail.com>
2024-01-20 19:01:23 +0530

    inbuilt arr function program

---
+++ Countingsort.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Countingsort {
+    public static void printarr(Integer arr[]){
+        for(Integer i = 0; i<arr.length; i++){
+            System.out.println(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+      // Use print instead of println
+      Integer arr[] ={5,2,3,6,1};
+      Arrays.sort(arr);
+      printarr(arr);
+        
+       
+}
+}
